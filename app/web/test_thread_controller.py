a0ce386fa4e514d3c92c031502cf3949a99e1326
Anthony Legros <alegros@cirilgroup.com>
2022-10-10 23:24:14 +0200

    Adding/Modifiying a few tests

--- app/web/test_thread_controller.py
+++ app/web/test_thread_controller.py
@@ -1,12 +1,22 @@
 import httpx
 
-def test_put_thread():
+def test_put_thread_should_create_a_thread():
+    response = httpx.put("http://localhost/threads/", json={'title': 'my_title'})
+    # Assert HTTP code and if received document id is valid
+    assert response.status_code == 201
+    json_response = response.json()
+    assert list(json_response.keys()) == ['_id', 'title', 'comments']
+    del json_response['_id']
+    assert json_response == {'title': 'my_title', 'comments': []}
+
+def test_put_thread_should_be_idempotent():
+    httpx.put("http://localhost/threads/", json={'id': 'test', 'title': 'my_title', 'comments': 'my_comments'})
     response = httpx.put("http://localhost/threads/", json={'id': 'test', 'title': 'my_title', 'comments': 'my_comments'})
     # Assert HTTP code and if received document id is valid
     assert response.status_code == 201
     assert list(response.json().keys()) == ['_id', 'title', 'comments']
 
-def test_read_thread():
+def test_get_threads_should_return_a_list_of_threads():
     response = httpx.get("http://localhost/threads/")
     # Assert HTTP code and if received document id is valid
     assert response.status_code == 200
